1889d279c57ce9002f77d3831df8e2deeb5fc95e
lunarcwhite <rizkym058@gmail.com>
2023-03-01 07:54:19 +0700

    task take home

---
+++ nomber-1.js
@@ -0,0 +1,41 @@
+const angka1 = 10 * 80 + 10 + 37 + 62
+console.log(angka1)
+const angka2 = 100 - 10 + 20 - 80 -28
+console.log(angka2)
+const angka3 = 80 + 80 / 2 /2 /2
+console.log(angka3)
+const angka4 = 100 % 2 + 1 - 1 + 10
+console.log(angka4)
+const angka5 = 70 - 10 % 5 + 10 - 2
+console.log(angka5)
+const angka6 = 76 - 33 + 37 * 2 * 3
+console.log(angka6)
+const angka7 = 2 ** 2 * 8 + 10 / 2
+console.log(angka7)
+const angka8 = 72 - 72 + 2 ** 5 - 3
+console.log(angka8)
+const angka9 = 63 - 28 - 18 - 28 + 12  
+console.log(angka9)
+const angka10 = 82 + 12 % 2 + 1 +5
+console.log(angka10)
+const angka11 = 72 + 28 % 5 + 10 / 2
+console.log(angka11)
+const angka12 = 82 + 1 + 17 / 4 * 2
+console.log(angka12)
+const angka13 = 73 + 1 - 28 * 3 / 3
+console.log(angka13)
+const angka14 = 27 / 9 + 3 % 3 + 1
+console.log(angka14)
+const angka15 = 73 + 12 - 80 / 5 + 1
+console.log(angka15)
+const angka16 = 100 % 2 + 5 ** 3 + 125
+console.log(angka16)
+const angka17 = 80 - 80 + 100 % 5 + 10
+console.log(angka17)
+const angka18 = 50 - 4 - 18 + 2 * 4
+console.log(angka18)
+const angka19 = 70 * 7 / 2 + 3 - 100
+console.log(angka19)
+const angka20 = 1000 / 100 * 10 % 10 + 10
+console.log(angka20)
+
